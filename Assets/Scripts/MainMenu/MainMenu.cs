fbd0577d108b39ac11dee7ece3fe7b48048a06c2
Mikkel <mikkel.rasmussen@tobii.com>
2019-09-18 13:08:45 +0200

    added base main menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Button startButton = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
