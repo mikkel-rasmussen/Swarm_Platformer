b3987550d75a6f4c71ba043ec3ee0e8d652d92c4
Mikkel <mikkel.rasmussen@tobii.com>
2019-09-18 12:50:55 +0200

    added base powerup item which can be used with space

---
+++ Assets/Scripts/LightningPickupComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningPickupComponent : PickupComponentBase
+{
+    private float powerupDuration = 5f;
+
+
+    public override void Use(PlayerPowerup _poweruoComponent)
+    {
+        Debug.Log("Item used");
+        ParticleSystem ps = _poweruoComponent.GetComponent<ParticleSystem>();
+        var trails = ps.trails;
+
+        trails.enabled = true;
+
+        StartCoroutine(SwitchOff(ps));
+    }
+
+    private IEnumerator SwitchOff(ParticleSystem _ps)
+    {
+        float current = 0f;
+
+        while (current < 1f)
+        {
+            current += Time.deltaTime / powerupDuration;
+
+            yield return null;
+        }
+        var trails = _ps.trails;
+        trails.enabled = false;
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter/PlayerPowerup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowerup : MonoBehaviour
+{
+    IPickupComponent pickupItem = null;
+
+    private void Update()
+    {
+        if (pickupItem != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            pickupItem.Use(this);
+            pickupItem = null;
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (pickupItem != null) { return; }
+
+    //    if (collision.gameObject.GetComponent<IPickupComponent>() != null)
+    //    {
+    //        pickupItem = collision.gameObject.GetComponent<IPickupComponent>().Pickup();
+    //        Destroy(collision.gameObject.GetComponent<Renderer>());
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (pickupItem != null) { return; }
+
+        if (other.gameObject.GetComponent<IPickupComponent>() != null)
+        {
+            pickupItem = other.gameObject.GetComponent<IPickupComponent>().Pickup();
+            Destroy(other.gameObject.GetComponent<Renderer>());
+        }
+    }
+}
