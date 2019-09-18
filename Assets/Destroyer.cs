46438cc7db6e2036fc3982a097db42142ddb96cb
Yevhen Krupin (ykn) <ykn@tobii.com>
2019-09-18 12:50:41 +0300

    added walls material and destroyer

---
+++ Assets/Destroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
+            {
+                this.GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
+}
